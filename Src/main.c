24262d968b97c1ba743078133ce32b276f7f08ee
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2016-07-24 22:25:47 -0400

    *Added led toggle in default task to test the os

--- Src/main.c
+++ Src/main.c
@@ -340,7 +340,8 @@ void StartDefaultTask(void const * argument)
   /* Infinite loop */
   for(;;)
   {
-    osDelay(1);
+	HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
+    osDelay(1000);
   }
   /* USER CODE END 5 */ 
 }
